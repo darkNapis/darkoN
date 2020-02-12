44830fb288e5d8dc4ea37446290cbefe9950565d
darkNapis <60657967+darkNapis@users.noreply.github.com>
2020-02-12 12:40:04 +0100

    Add files via upload

--- homework_05/script/script.js
+++ homework_05/script/script.js
@@ -9,10 +9,14 @@ fetch('https:raw.githubusercontent.com/sedc-codecademy/sedc7-04-ajs/master/g2/Cl
     })
     .catch(error => console.error(error))
 
-    let allMaleStudentsNotFromSkopje = students => students.filter(s => s.gender === 'Male' && s.city !== 'Skopje');
+    let everyStudentsLog = students => students.forEach(s => {
+        console.log(`${s.firstName} ${s.lastName} is coming form ${s.city}, 
+        he is ${s.age} years old and he is ${s.averageGrade === 1 ? 'Failing' : 'Passing'} the class.`);
+    });
+    let allMaleStudentsNotFromSkopje = students => 
+                            students.filter(s => s.gender === 'Male' && s.city !== 'Skopje');
     let allFemaleAdults = students => students.filter(s => s.gender === 'Female' && s.age > 18);
-    let avgAge = students => students.map(s => s.age).reduce((sum, avg) => sum + avg / students.length, 0);
-    let allFemaleStudentsLastNameWIthA = students => students.filter(s => s.gender === 'Female' && s.lastName.endsWith('a'));
-    let everyStudentsLog = students => students.filter(s => s.averageGrade)
-                                               .map(s => `${s.firstName} ${s.lastName} is coming form ${s.city}, he is ${s.age} years old and he is ${s.averageGrade === 1 ? 'Failing' : 'Passing'} the class.`);
-                                    
+    let avgAge = students => students.map(s => s.age).reduce((sum, avg) => sum + avg, 0) / students.length;
+    let allFemaleStudentsLastNameWIthA = students => 
+                            students.filter(s => s.gender === 'Female' && s.lastName.endsWith('a'));
+    
