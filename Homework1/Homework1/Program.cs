f776fe5072b156bd592370770ba6f45b9cbf36ba
darkNapis <60657967+darkNapis@users.noreply.github.com>
2020-03-04 14:16:05 +0100

    Add files via upload

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("STOP");
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("READY");
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("GO");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
